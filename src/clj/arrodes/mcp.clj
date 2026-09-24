02b828ce7f8119bf666df6e3eb8ac8e58ab919eb
DeadMeme <45178623+DeadMeme5441@users.noreply.github.com>
2026-09-24 18:05:02 +0530

    release: prepare v0.1.6 (#11)

--- src/clj/arrodes/mcp.clj
+++ src/clj/arrodes/mcp.clj
@@ -344,7 +344,7 @@
     (try
       (let [mcp-client
             (client/make-client
-             {:info (entity-support/make-info "Arrodes" "0.1.5" "Arrodes headless runtime")
+             {:info (entity-support/make-info "Arrodes" "0.1.6" "Arrodes headless runtime")
               :client-transport transport
               :traffic-logger traffic/nop-traffic-logger
               :print-banner? false
